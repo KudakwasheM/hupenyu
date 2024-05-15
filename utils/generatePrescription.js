7ba85071145624cfac041076ef752d697342b7c1
Kudakwashe Masaya <masayakudakwashe@gmail.com>
2024-05-15 20:36:04 +0200

    Update generatePrescription.js

--- utils/generatePrescription.js
+++ utils/generatePrescription.js
@@ -2,20 +2,206 @@ var PdfPrinter = require("pdfmake");
 var fs = require("fs");
 const moment = require("moment");
 
-const makePrescription=(data)=>{
-    var fonts = {
-        Roboto: {
-          normal: "fonts/Roboto-Regular.ttf",
-          bold: "fonts/Roboto-Medium.ttf",
-          italics: "fonts/Roboto-Italic.ttf",
-          bolditalics: "fonts/Roboto-MediumItalic.ttf",
-        },
-      };
-      var printer = new PdfPrinter(fonts);
+const makePrescription = (data) => {
+  var fonts = {
+    Roboto: {
+      normal: "fonts/Roboto-Regular.ttf",
+      bold: "fonts/Roboto-Medium.ttf",
+      italics: "fonts/Roboto-Italic.ttf",
+      bolditalics: "fonts/Roboto-MediumItalic.ttf",
+    },
+  };
+  var printer = new PdfPrinter(fonts);
 
-      const tableRows = [];
+  const tableRows = [];
 
-      data.medication.foreach(m=>{
-        const {name,}
-      })
-}
+  data.medication.foreach((m) => {
+    const { medicine, dosage, frequency, days } = m;
+    tableRows.push([
+      {
+        text: medicine,
+        border: [false, false, false, false],
+        margin: [0, 5, 0, 5],
+        alignment: "left",
+      },
+      {
+        text: dosage,
+        border: [false, false, false, false],
+        margin: [0, 5, 0, 5],
+        alignment: "left",
+      },
+      {
+        text: frequency,
+        border: [false, false, false, false],
+        margin: [0, 5, 0, 5],
+        alignment: "left",
+      },
+      {
+        text: days,
+        border: [false, false, false, false],
+        margin: [0, 5, 0, 5],
+        alignment: "left",
+      },
+    ]);
+  });
+
+  const table = {
+    layout: "lightHorizontalLines",
+    table: {
+      headerRows: 1,
+      widths: ["*", "auto", "auto", "auto"],
+      body: [
+        [
+          {
+            text: "SERVICE/MEDICATION",
+            fillColor: "#eaf2f5",
+            border: [false, true, false, true],
+            margin: [0, 5, 0, 5],
+            textTransform: "uppercase",
+          },
+          {
+            text: "QUANTITY",
+            border: [false, true, false, true],
+            alignment: "right",
+            fillColor: "#eaf2f5",
+            margin: [0, 5, 0, 5],
+            textTransform: "uppercase",
+          },
+          {
+            text: "UNIT PRICE",
+            border: [false, true, false, true],
+            alignment: "right",
+            fillColor: "#eaf2f5",
+            margin: [0, 5, 0, 5],
+            textTransform: "uppercase",
+          },
+          {
+            text: "TOTAL",
+            border: [false, true, false, true],
+            alignment: "right",
+            fillColor: "#eaf2f5",
+            margin: [0, 5, 0, 5],
+            textTransform: "uppercase",
+          },
+        ],
+        ...tableRows,
+      ],
+    },
+  };
+  var docDefinition = {
+    content: [
+      {
+        columns: [
+          {
+            text: "LOGO",
+            padding: [10, 15],
+            fontSize: 30,
+            bold: true,
+            backgroundColor: "#eaf2f5",
+          },
+          [
+            {
+              text: "Prescription",
+              color: "#333333",
+              width: "*",
+              fontSize: 28,
+              bold: true,
+              alignment: "right",
+              margin: [0, 0, 0, 15],
+            },
+            {
+              stack: [
+                {
+                  columns: [
+                    {
+                      text: "Prescription No.",
+                      color: "#aaaaab",
+                      bold: true,
+                      width: "*",
+                      fontSize: 12,
+                      alignment: "right",
+                    },
+                    {
+                      text: `${data.prescription_number}`,
+                      bold: true,
+                      color: "#333333",
+                      fontSize: 12,
+                      alignment: "right",
+                      width: 100,
+                    },
+                  ],
+                },
+                {
+                  columns: [
+                    {
+                      text: "Date Issued",
+                      color: "#aaaaab",
+                      bold: true,
+                      width: "*",
+                      fontSize: 12,
+                      alignment: "right",
+                    },
+                    {
+                      text: moment(data.created_at).format("MMM DD, YYYY"),
+                      bold: true,
+                      color: "#333333",
+                      fontSize: 12,
+                      alignment: "right",
+                      width: 100,
+                    },
+                  ],
+                },
+                // {
+                //   columns: [
+                //     {
+                //       text: "Status",
+                //       color: "#aaaaab",
+                //       bold: true,
+                //       fontSize: 12,
+                //       alignment: "right",
+                //       width: "*",
+                //     },
+                //     {
+                //       text: data.paymentStatus.toUpperCase(),
+                //       bold: true,
+                //       fontSize: 12,
+                //       alignment: "right",
+                //       color: data.paymentStatus === "paid" ? "green" : "red",
+                //       width: 100,
+                //     },
+                //   ],
+                // },
+              ],
+            },
+          ],
+        ],
+      },
+      {
+        width: "100%",
+        alignment: "center",
+        text: `Invoice No. ${data.prescription_number}`,
+        bold: true,
+        margin: [0, 10, 0, 10],
+        fontSize: 15,
+      },
+      table,
+    ],
+    styles: {
+      header: {
+        fontSize: 18,
+        bold: true,
+        margin: [0, 0, 0, 10],
+      },
+    },
+  };
+
+  var pdfDoc = printer.createPdfKitDocument(docDefinition);
+  pdfDoc.pipe(
+    fs.createWriteStream(
+      `${process.env.FILE_UPLOAD_PATH}/prescriptions/${data.prescription_number}.pdf`
+    )
+  );
+  pdfDoc.end();
+};
+
+module.exports = { makePrescription };
