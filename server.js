41557321adfceaf8a13fa8d30719ab00e398eb1e
Kudakwashe Masaya <masayakudakwashe@gmail.com>
2024-06-25 09:49:17 +0200

    Update server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.use(fileupload());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://chipatara.vercel.app/");
+  res.setHeader("Access-Control-Allow-Origin", "https://chipatara.vercel.app");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, OPTIONS"
